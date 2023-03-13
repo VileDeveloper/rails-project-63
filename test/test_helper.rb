c96a8fdaf7974cd79327316a37411b9be716bdf8
VileDeveloper <vladimirlitov131127@gmail.com>
2023-03-13 12:47:25 +0600

    refacor(.rubocop.yml): fix last rubocop warning
    Added for hexlet ruby course step 6

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Dir[File.expand_path('../lib/**/*.rb', __dir__)].sort.each { |file| require file }
+Dir[File.expand_path('../lib/**/*.rb', __dir__)].each { |file| require file }
 
 require 'minitest/autorun'
 require 'minitest/power_assert'
